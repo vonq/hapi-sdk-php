f673f0cc754c9f16d313a5af264b70f64a5158da
Marian Mazarovici <marianmazarovici@users.noreply.github.com>
2022-09-13 19:26:14 +0100

    Update docs (#5)
    Co-authored-by: Marian Mazarovici <git@apimatic.io>

---
+++ src/Models/PaginatedProductListWithoutFacetsModel.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * HAPI
+ *
+ * This file was automatically generated for VONQ by APIMATIC v3.0 ( https://www.apimatic.io ).
+ */
+
+namespace HAPI\Models;
+
+use stdClass;
+
+class PaginatedProductListWithoutFacetsModel implements \JsonSerializable
+{
+    /**
+     * @var int
+     */
+    private $count;
+
+    /**
+     * @var string|null
+     */
+    private $next;
+
+    /**
+     * @var string|null
+     */
+    private $previous;
+
+    /**
+     * @var ProductModel[]
+     */
+    private $results;
+
+    /**
+     * @param int $count
+     * @param ProductModel[] $results
+     */
+    public function __construct(int $count, array $results)
+    {
+        $this->count = $count;
+        $this->results = $results;
+    }
+
+    /**
+     * Returns Count.
+     * Total number of products.
+     */
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * Sets Count.
+     * Total number of products.
+     *
+     * @required
+     * @maps count
+     */
+    public function setCount(int $count): void
+    {
+        $this->count = $count;
+    }
+
+    /**
+     * Returns Next.
+     * Path to the next page of results, if any.
+     */
+    public function getNext(): ?string
+    {
+        return $this->next;
+    }
+
+    /**
+     * Sets Next.
+     * Path to the next page of results, if any.
+     *
+     * @maps next
+     */
+    public function setNext(?string $next): void
+    {
+        $this->next = $next;
+    }
+
+    /**
+     * Returns Previous.
+     * Path to the previous page of results, if any.
+     */
+    public function getPrevious(): ?string
+    {
+        return $this->previous;
+    }
+
+    /**
+     * Sets Previous.
+     * Path to the previous page of results, if any.
+     *
+     * @maps previous
+     */
+    public function setPrevious(?string $previous): void
+    {
+        $this->previous = $previous;
+    }
+
+    /**
+     * Returns Results.
+     *
+     * @return ProductModel[]
+     */
+    public function getResults(): array
+    {
+        return $this->results;
+    }
+
+    /**
+     * Sets Results.
+     *
+     * @required
+     * @maps results
+     *
+     * @param ProductModel[] $results
+     */
+    public function setResults(array $results): void
+    {
+        $this->results = $results;
+    }
+
+    private $additionalProperties = [];
+
+    /**
+     * Add an additional property to this model.
+     *
+     * @param string $name Name of property
+     * @param mixed $value Value of property
+     */
+    public function addAdditionalProperty(string $name, $value)
+    {
+        $this->additionalProperties[$name] = $value;
+    }
+
+    /**
+     * Encode this object to JSON
+     *
+     * @param bool $asArrayWhenEmpty Whether to serialize this model as an array whenever no fields
+     *        are set. (default: false)
+     *
+     * @return array|stdClass
+     */
+    #[\ReturnTypeWillChange] // @phan-suppress-current-line PhanUndeclaredClassAttribute for (php < 8.1)
+    public function jsonSerialize(bool $asArrayWhenEmpty = false)
+    {
+        $json = [];
+        $json['count']    = $this->count;
+        $json['next']     = $this->next;
+        $json['previous'] = $this->previous;
+        $json['results']  = $this->results;
+        $json = array_merge($json, $this->additionalProperties);
+
+        return (!$asArrayWhenEmpty && empty($json)) ? new stdClass() : $json;
+    }
+}
